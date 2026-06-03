cdfaa3b7a7ddefc87815f34cc95bfe0be94e963f
Arantza Lizarraga Somoza <2023030578@upsin.edu.mx>
2026-06-03 11:42:39 -0700

    Restore main tabs header and Inicio spacing

---
+++ src/app/escanear/escanear.page.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonButton, IonContent, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { cameraOutline, imageOutline, sparkles, trashOutline } from 'ionicons/icons';
+
+@Component({
+  selector: 'app-escanear',
+  templateUrl: 'escanear.page.html',
+  styleUrls: ['escanear.page.scss'],
+  imports: [CommonModule, IonButton, IonContent, IonIcon],
+})
+export class EscanearPage {
+  capturaReciente = true;
+  nombreCaptura = 'Ticket_20231024_123.jpg';
+
+  constructor(private readonly router: Router) {
+    addIcons({ cameraOutline, imageOutline, sparkles, trashOutline });
+  }
+
+  tomarFoto(): void {
+    this.capturaReciente = true;
+  }
+
+  abrirGaleria(): void {
+    this.capturaReciente = true;
+  }
+
+  eliminarCaptura(): void {
+    this.capturaReciente = false;
+  }
+
+  analizarConIA(): void {
+    if (!this.capturaReciente) {
+      return;
+    }
+
+    this.router.navigate(['/confirmar-gasto']);
+  }
+}
