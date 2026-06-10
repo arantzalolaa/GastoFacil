a24dd4f59b1e8453f516fc976334fb8b7e45f43e
Arantza <2023030578@upsin.edu.mx>
2026-06-10 08:14:43 -0700

    Auto-generated commit

---
+++ chunk-B6QI2HY3.js
@@ -0,0 +1,1 @@
+var s={production:!0,supabaseUrl:"https://dlczicgfetwvywjyswfu.supabase.co",supabaseKey:"sb_publishable_gCwGqFdx3Yv7UYns3pB3HQ_USn6EVan",geminiApiKey:"AQ.Ab8RN6Jo56Oi5bmmN9Su-buyLZVJwhYGELFH4sDkJH4Mvfzzrw"};export{s as a};
